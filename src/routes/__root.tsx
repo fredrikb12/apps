8132a3d5cd0d801221ef4141663aa66ac9d3b5c5
Fredrik Bäckman <fredrik.backman12@gmail.com>
2025-10-26 00:11:19 +0200

    Fix nav

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
-import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { Navigate, Outlet, createRootRoute } from "@tanstack/react-router";
 import Navbar from "../components/Navbar";
 
 export const Route = createRootRoute({
   component: RootComponent,
+  notFoundComponent: () => <Navigate to="/todos" />,
 });
 
 function RootComponent() {
